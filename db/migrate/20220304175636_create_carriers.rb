671d082c1bf6ec13c79d764c735e0336331c45a6
aschonely83 <aschonely@gmail.com>
2022-03-04 12:59:42 -0500

    add carriers

---
+++ db/migrate/20220304175636_create_carriers.rb
@@ -0,0 +1,10 @@
+class CreateCarriers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :carriers do |t|
+      t.string :name
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end

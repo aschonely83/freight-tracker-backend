35b3145e3daf1e391e376ec56dad7bbc4e96b6b9
aschonely83 <aschonely@gmail.com>
2022-03-04 13:30:30 -0500

    created seed data

--- app/models/carrier.rb
+++ app/models/carrier.rb
@@ -1,3 +1,4 @@
 class Carrier < ApplicationRecord
   belongs_to :user
+  has_many :pallets
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.first 
+
+NFI = user.carriers.find_or_create_by(name: "NFI")
+
+pallet_1 = user.pallets.find_or_create_by(date: "03/04/2021", scheduled: 6, picked_up: 6, carrier_id: NFI.id)
